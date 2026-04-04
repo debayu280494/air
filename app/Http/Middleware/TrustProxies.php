767653b29a8b1bee46a5136b836bd5dbddf5ba02
bayukazeto <itkaryasatria@gmail.com>
2026-04-04 12:49:05 +0700

    kernel

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust semua proxy (penting untuk Docker / EasyPanel)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Header yang digunakan untuk detect HTTPS
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (env('FORCE_HTTPS', false)) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
     }
